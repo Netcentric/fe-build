1e4659239de465056af72f5b6aed4471f260c0bd
Zoran <markonimobile@gmail.com>
2023-06-19 12:26:59 +0200

    feature/#31-upgrade-to-webpack-5 switch to eslint-webpack-plugin

--- config/eslint.config.js
+++ config/eslint.config.js
@@ -1,14 +1,22 @@
-// Webpack configuration
+const ESLintPlugin = require('eslint-webpack-plugin');
+
 module.exports = {
-  enforce: 'pre',
-  test: /\.js$/,
-  exclude: /node_modules/,
-  use: {
-    loader: 'eslint-loader',
-    options: {
-      cache: true,
-      failOnError: true,
-      fix: true
-    }
-  }
+  // ...
+  plugins: [new ESLintPlugin(options)],
+  // ...
 };
+
+// Webpack configuration
+// module.exports = {
+//   enforce: 'pre',
+//   test: /\.js$/,
+//   exclude: /node_modules/,
+//   use: {
+//     loader: 'eslint-loader',
+//     options: {
+//       cache: true,
+//       failOnError: true,
+//       fix: true
+//     }
+//   }
+// };
