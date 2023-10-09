88ceb17916c08d0229f36c3561baac52ea8e7f66
Felipe Simoes <felipe.simoes@netcentric.biz>
2023-10-09 15:48:33 +0200

    #97 > update normalized path on write file

--- utils/writeFile.js
+++ utils/writeFile.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
 const { log } = require('./log');
+const path = require('path');
 
 module.exports = function writeFile(filepath, content, override = false) {
+  const normalizePath = path.normalize(filepath);
   // if is not set to not override
-  if (!override && fs.existsSync(filepath)) return;
+  if (!override && fs.existsSync(normalizePath)) return;
 
   // white it
-  fs.writeFileSync(filepath, content,
+  fs.writeFileSync(normalizePath, content,
     err => log(__filename, 'error', `could not write file: ${err.Error},'\n ${err.path}`, 'error'));
 
-  log(__filename, `write file...${filepath}`, '', 'info');
+  log(__filename, `write file...${normalizePath}`, '', 'info');
 };
