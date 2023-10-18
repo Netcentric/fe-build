4924075beec4a740f6f4a40f66c929f4665c67fd
Felipe Simoes <felipe.simoes@netcentric.biz>
2023-10-18 18:55:30 +0200

    BREAKING CHANGE: Upgrate dependencies and tools to lastest versions

--- utils/checkChunk.js
+++ utils/checkChunk.js
@@ -3,23 +3,17 @@ const includesInModules = (names = []) => (module) => names.filter((name) => mod
 // To check if a context is from a vendor
 module.exports = function checkChunk(module, excludes = [], includes = []) {
   // is not a external module so there is no context
-  console.log('has module', module, 'excludes', excludes, 'includes', includes);
   if (!module) {
-    console.log('no module');
     return false;
   }
   
   if (includesInModules(excludes)(module)) {
-    console.log('is excluded');
     return false;
   }
   // has includes defined and the module is not in the includes
   if (includes.length === 0 ) {
-    console.log('no includes');
     return true;
   }
-
-  console.log('is includes', includesInModules(includes)(module));
 
   return includesInModules(includes)(module);
 };
