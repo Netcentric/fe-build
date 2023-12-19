27914627bee3ceebeb6454760b3b9ef8135fce11
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2023-12-19 18:38:57 +0100

    [-] Add webpack tests

--- test/src/publish/publish.src.js
+++ test/src/publish/publish.src.js
@@ -4,11 +4,15 @@ class MainPublish {
   constructor() {
     this.value = randomInt(1, 10);
     this.arr = [...[0, 1, 2]];
+    this.obj = {
+      name: 'test'
+    };
     this.init();
   }
 
   init() {
-    console.log(`Hello World ${this.value}!`);
+    const { name } = this.obj;
+    console.log(`Hello World ${this.value}! ${name}`);
   }
 }
 export const instace = new MainPublish();
