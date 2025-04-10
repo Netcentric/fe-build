d533847b900b22abf126e93bb3fc66d00efc1759
hugo <hugo.estevez@netcentric.biz>
2025-04-10 13:44:21 +0200

    fix(utils): Fix category name generation to prevent consecutive dots and leading dots

--- utils/getClientlib.js
+++ utils/getClientlib.js
@@ -6,7 +6,10 @@ module.exports = function getClientlib(original, config) {
   const folder = path.dirname(original);
   const fileName = path.basename(original);
   const clear = new RegExp([...sourceTypes, sourceKey, bundleKey].join('|'), 'gi');
-  const name = folder.replace(clear, '').split(path.sep).join('.');
+  const name = folder.replace(clear, '')
+    .split(path.sep)
+    .filter((categoryItem, idx) => categoryItem && (idx !== 0 || categoryItem.charAt(0) !== '.'))
+    .join('.');
 
   return {
     folder,
