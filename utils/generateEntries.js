81a7229757568abea848f9aaddb8eb055445eaf2
hugo <hugo.estevez@netcentric.biz>
2025-05-07 17:23:48 +0200

    feat(config): Support clientlib metadata generation for dynamically created CSS files

--- utils/generateEntries.js
+++ utils/generateEntries.js
@@ -1,9 +1,9 @@
 const glob = require('fast-glob');
 const path = require('path');
 
-module.exports = function generateEntries(config, extension = 'js') {
-  const sourcePattern = `**/*.${config.general.sourceKey}.${extension}`;
-  const sourcesFiles = glob.sync(sourcePattern, { cwd: config.general.sourcesPath });
+module.exports = function generateEntries(config, extension = 'js', filenamePattern = config.general.sourceKey, cwd = config.general.sourcesPath) {
+  const sourcePattern = `**/*.${filenamePattern}.${extension}`;
+  const sourcesFiles = glob.sync(sourcePattern, { cwd: cwd });
 
   // if is multiple entries
   if (config && config.general && config.general.multiple) {
@@ -12,9 +12,9 @@ module.exports = function generateEntries(config, extension = 'js') {
     sourcesFiles.forEach((file) => {
       const dir = path.dirname(file);
       const fileName = path.basename(file);
-      const destFileName = fileName.replace(config.general.sourceKey, config.general.bundleKey);
+      const destFileName = fileName.replace(filenamePattern, config.general.bundleKey);
       const destFile = path.join(dir, destFileName);
-      sources[destFile] = path.join(config.general.sourcesPath, file);
+      sources[destFile] = path.join(cwd, file);
     });
 
     return sources;
